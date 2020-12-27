5c24ec51f2a02e8e1812f1b104770693f80f61cb
rickylai <f5x2b@ugrad.cs.ubc.ca>
2020-12-26 18:04:43 -0800

    Implemented the Clock visualization program after importing from tkinter and time

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-msg = "Hello World"
-print(msg)
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+# File initialization testing statements
+#msg = "Hello World"
+#print(msg)
+#
+
+root = Tk();
+root.title("PythonClock")
+
+def findTime():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root, font=("Times", 24), background = "white", foreground = "black")
+label.pack(anchor='center')
+findTime()
+
+mainloop()
